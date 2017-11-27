c2dbd95012363a4ab590e00a2403651ca5a83db5
Jrobertzz <robertsjw2@vcu.edu>
2017-11-27 17:19:14 -0500

    testing for vue

--- frontend/serviceBrokerWeb.py
+++ frontend/serviceBrokerWeb.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import subprocess
 from OpenSSL import SSL
 import MySQLdb
@@ -10,29 +10,19 @@ db = MySQLdb.connect(host="148.100.99.53",      # your host(manager node)
                      user="root",           # username
                      passwd="pass",         # password
                      db="ServiceBroker")       # name of database
-c = db.cursor()
 
-def runCMD(cmd):
-    subprocess.Popen(cmd, shell=True)
-    return 'test'
+def query_db():
+    cur = db.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute('select * from services')
+    cache = cur.fetchall()
+    cur.connection.close()
+    return cache
 
-def action(serviceData):
-    if serviceData[1] == "CMD":
-	return runCMD(serviceData[0])
-    else:
-	return False
+cache = query_db()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-	c.execute("select service_name from services")
-        serviceData = list(c.fetchall())
-	for i in range(0, len(serviceData)):
-	    serviceData[i] = serviceData[i][0]
-        return render_template("test.html", services=serviceData)
-    else:
-	c.execute("select service_command, service_type from services where service_name=%s", [request.form['docker']])
-	serviceData = c.fetchone()
-        return request.form['docker']+" ran successfully and output was "+action(serviceData)
+        return jsonify(cache)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=443, ssl_context=('./certs/cert.pem', './certs/key.pem'))
