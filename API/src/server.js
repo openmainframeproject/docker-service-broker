c27a5bf56e9ac87ef4b4db8cc6e266d4c677daa0
Jrobertzz <robertsjw2@vcu.edu>
2018-02-08 16:58:52 -0500

    API returns contents of services.services from database

--- API/src/server.js
+++ API/src/server.js
@@ -1,26 +1,24 @@
+//needs refactoring
+
 var express = require("express");
 var mysql = require("mysql");
 var bodyParser = require("body-parser");
 var app = express();
 
 var con = mysql.createConnection({
-    host: 'localhost',
+    host: '148.100.98.185',
     user: 'root',
     password: 'pass',
     database: 'services'
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-	var serice_list = '';
-	con.query('SELECT * FROM services')
-	{
-        if (err) throw err;
-        serice_list = result;
-    };
-    res.json({"test" : "get"});
-})
+app.get("/",function(req,res) {
+    con.query("SELECT * FROM services", function (err, result, fields) {
+        res.send(result);
+    });
+});
 
 app.listen(3000);
