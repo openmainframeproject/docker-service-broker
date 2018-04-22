1178c76758d691fa433aabe6b92df98e92226bcf
Jrobertzz <robertsjw2@vcu.edu>
2018-04-22 18:02:40 -0400

    create mysql directory

--- scripts/setupsql.sh
+++ scripts/setupsql.sh
@@ -1,3 +1,4 @@
+mkdir /data/mysql
 mysqldb="$(docker ps --format '{{.Names}}' | grep 'sql')"
 docker exec -i $(docker service ls | grep sql | awk '{print $1;}') mysql -h 127.0.0.1 -uroot -ppass <<< "create database ServiceBroker;"
 docker exec -i "$mysqldb" mysql -uroot -ppass ServiceBroker < /data/docker-service-broker/scripts/create.sql
